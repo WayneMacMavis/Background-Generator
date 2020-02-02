94e2295f71d4a7402cf3b57a4bb49fbb5813b407
Wayne <w.macmavis83@gmail.com>
2020-02-02 15:23:37 +0200

    Adding final touches

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var random = document.querySelector(".random");
+
 
 function setGradient() {
 	body.style.background = 
@@ -10,9 +12,25 @@ function setGradient() {
 	+ ", " 
 	+ color2.value 
 	+ ")";
-
 	css.textContent = body.style.background + ";";
 }
 
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i=0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function setRandomColor (e) {
+	if (e.target.matches(".random")) {
+		color1.value = getRandomColor();
+		color2.value = getRandomColor();
+		setGradient();
+	  }
+}
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+random.addEventListener("click", setRandomColor);
